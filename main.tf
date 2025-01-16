2e43d09301b2dd2da91aa5dba28887219c08a33d
Dan Sniderman <dan.sniderman@3cloudsolutions.com>
2025-01-16 16:13:43 -0600

    recreate module declaration

--- main.tf
+++ main.tf
@@ -4,8 +4,7 @@ provider "aws" {
 
 module "s3-webapp" {
   source  = "app.terraform.io/dan-sniderman-3cloud-tutorials/s3-webapp/aws"
-  name    = var.name
+  version = "1.0.0"
   region  = var.region
   prefix  = var.prefix
-  version = "1.0.0"
 }
